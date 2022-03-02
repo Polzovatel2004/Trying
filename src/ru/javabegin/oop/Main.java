0afffd26fa39c71be758756af8221d9774adeac0
Dmitriy <termaltforclash2@gmail.com>
2022-03-02 21:39:40 +0300

    Add sum of x and y (2)

--- src/ru/javabegin/oop/Main.java
+++ src/ru/javabegin/oop/Main.java
@@ -3,8 +3,14 @@ package ru.javabegin.oop;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+
+        int x = 50;
+        int y = 20;
+
+        int sum = x + y;
+        System.out.println("Sum of x and y is " + sum);
     }
 }
